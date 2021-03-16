552c2f69b3ab9c83bce62e8c4c322b09a7c9f5c6
Ruben Kuilder <rubenkuilder@gmail.com>
2021-03-16 11:20:00 +0100

    Cleaned up main class

--- src/main/kotlin_code/main.kt
+++ src/main/kotlin_code/main.kt
@@ -15,13 +15,13 @@ fun main(args: Array<String>) {
                 TransactionHandler()
             }
             2 -> {
-                println("Button 2 pressed");
+                println("Button 2 pressed")
             }
             3 -> {
-                println("Button 3 pressed");
+                println("Button 3 pressed")
             }
             4 -> {
-                println("Button 4 pressed");
+                println("Button 4 pressed")
             }
             else -> {
                 println("This is not an option.")
@@ -31,12 +31,12 @@ fun main(args: Array<String>) {
 }
 
 private fun printMenu() {
-    println("Main Menu:");
-    println("--------------");
-    println("1. Show transactions");
-    println("2. Make check");
-    println("3. Alter savings");
-    println("4. Pub Crawl");
-    println("--------------");
-    println("Enter your choice:");
+    println("Main Menu:")
+    println("--------------")
+    println("1. Show transactions")
+    println("2. Make check")
+    println("3. Alter savings")
+    println("4. Pub Crawl")
+    println("--------------")
+    println("Enter your choice:")
 }
